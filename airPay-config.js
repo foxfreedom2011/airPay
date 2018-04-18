988249cbbbddab009c8bd41a3be559ffac9e2360
SiqingIII@163.com <cqing@cqing.local>
2018-04-18 21:25:25 +0800

    update

--- airPay-config.js
+++ airPay-config.js
@@ -11,7 +11,7 @@ exports.biz_function = function(){
     
     biz_content = {
         body:'这是测试数据',
-        subject:'陌生人饭局v2.0',
+        subject:'测试商品名称',
         // 交易单号；
         out_trade_no:outTradeId,
 
